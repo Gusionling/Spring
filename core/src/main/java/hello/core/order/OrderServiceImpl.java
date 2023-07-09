b563f302e87e415e1cd8891ba44c9a735402b9c6
David <isdnhk@naver.com>
2023-07-09 18:02:40 +0900

    중복 빈 문제 해결

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,13 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     //final이 되어있으면 생성자를 사용하여 할당이 되어야한다.
     //인터페이스에만 의존하고 있고 구체적인 클래스에는 의존하고 있지 않다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
