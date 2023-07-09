f7b45dce7bfc503ef48a6fb3057ae961a7b3352b
David <isdnhk@naver.com>
2023-07-09 20:48:36 +0900

    PostConstruct로 리펙토링

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient {  
 
     private String url;
 
@@ -27,13 +30,13 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
-
+    @PostConstruct
     public void init()  {
-        connect();    
+        connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() {
         disconnect();
     }
