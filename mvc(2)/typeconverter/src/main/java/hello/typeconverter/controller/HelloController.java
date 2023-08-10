f67f4968b128df1e3bbfe9accefcc1804bb5a014
David <isdnhk@naver.com>
2023-08-10 17:19:56 +0900

    test : converter test 추가

---
+++ mvc(2)/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,25 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer invValue = Integer.valueOf(data);
+        System.out.println("invValue = " + invValue);
+        return "OK";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
