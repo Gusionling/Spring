278f3059a0b2b8a4007782ec011fe035416c07e4
David <isdnhk@naver.com>
2023-09-21 14:17:11 +0900

    test: 잔고부족 Test

--- springtx/src/test/java/hello/springtx/order/OrderServiceTest.java
+++ springtx/src/test/java/hello/springtx/order/OrderServiceTest.java
@@ -1,9 +1,13 @@
 package hello.springtx.order;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Or;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,45 @@ class OrderServiceTest {
     @Autowired OrderRepository orderRepository;
 
     @Test
-    void order() {
+    void order() throws NotEnoughMoneyException {
+
+        Order order = new Order();
+        order.setUsername("정상");
+
+        orderService.order(order);
+
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        Assertions.assertThat(findOrder.getPayStatus()).isEqualTo("완료");
+    }
+
+    @Test
+    void runtimeException() throws NotEnoughMoneyException {
+
+        Order order = new Order();
+        order.setUsername("예외");
+
+        Assertions.assertThatThrownBy(() -> orderService.order(order))
+                .isInstanceOf(RuntimeException.class);
+
+
+        Optional<Order> orderOptional = orderRepository.findById(order.getId());
+        Assertions.assertThat(orderOptional.isEmpty()).isTrue();
+    }
+
+    @Test
+    void bizException() {
+
+        Order order = new Order();
+        order.setUsername("잔고부족");
+
+        try {
+            orderService.order(order);
+        } catch (NotEnoughMoneyException e) {
+            log.info("고객에게 잔고 부족을 알리고 별도의 계좌로 입금하도록 안내");
+        }
+
+
+        Order findOrder = orderRepository.findById(order.getId()).get();
+        Assertions.assertThat(findOrder.getPayStatus()).isEqualTo("대기");
     }
 }
