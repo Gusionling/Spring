83097fe4b02a7a8acee9dbc153c7f2f0c8d8b15a
David <isdnhk@naver.com>
2023-10-03 11:51:52 +0900

    add : spring security 환경설정

---
+++ security1/src/main/java/hk/security1/controller/IndexController.java
@@ -0,0 +1,15 @@
+package hk.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //view를 리턴하겠다!!
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index(){
+        //머스테치 기본폴더 src/main/resources/
+        //뷰리졸버 설정 : templates (prefix), .mustache(suffix)
+        return "index";
+    }
+}
