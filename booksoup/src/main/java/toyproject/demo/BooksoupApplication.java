082cff9a341c535c9b5b5059a452ca8221644ed3
Gusionling <isdnhk@naver.com>
2024-10-10 12:52:41 +0900

    add : project update + postgreSQL 연결

---
+++ booksoup/src/main/java/toyproject/demo/BooksoupApplication.java
@@ -0,0 +1,13 @@
+package toyproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksoupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksoupApplication.class, args);
+	}
+
+}
