744be03f283b596491c75459db58ce329d49a992
Gusionling <isdnhk@naver.com>
2024-10-24 21:04:05 +0900

    feat : project initialize

---
+++ preNews/src/main/java/com/example/preNews/PreNewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.preNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreNewsApplication.class, args);
+	}
+
+}
