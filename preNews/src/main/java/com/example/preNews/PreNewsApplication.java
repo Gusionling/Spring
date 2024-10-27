feb4ab8165985c9b4f3e4fe1b3b6950bdae9a030
Gusionling <isdnhk@naver.com>
2024-10-27 21:18:57 +0900

    feat : @EnableJpaAuditing을 사용하여 엔티티 생성, 수정 자동 기록

--- preNews/src/main/java/com/example/preNews/PreNewsApplication.java
+++ preNews/src/main/java/com/example/preNews/PreNewsApplication.java
@@ -2,8 +2,10 @@ package com.example.preNews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PreNewsApplication {
 
 	public static void main(String[] args) {
--- preNews/src/main/java/com/example/preNews/domain/User.java
+++ preNews/src/main/java/com/example/preNews/domain/User.java
@@ -5,10 +5,14 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user")
@@ -31,8 +35,14 @@ public class User {
     @Column(name = "is_login", columnDefinition = "TINYINT(1)")
     private Boolean isLogin;
 
+    @CreatedDate
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "created_at")
+    private LocalDateTime updatedAt;
+
 
     @Builder
     public User(Long serialId) {
